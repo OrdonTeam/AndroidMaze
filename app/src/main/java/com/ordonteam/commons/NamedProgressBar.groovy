c58a94e73982de02d18b38efde459b3f08afa65c
Kala <kala009@wp.pl>
2014-09-28 14:54:05 +0200

    CustomLayout and NamedProgressBar

---
+++ app/src/main/java/com/ordonteam/commons/NamedProgressBar.groovy
@@ -0,0 +1,55 @@
+package com.ordonteam.commons
+
+import android.content.Context
+import android.widget.LinearLayout
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+/**
+ * Created by Kala on 2014-09-28.
+ */
+public class NamedProgressBar extends LinearLayout{
+    public TextView header
+    public SeekBar seekBar
+    public int value
+    public int barSize = 400
+    private String name
+
+    public NamedProgressBar(Context context, String name, int maxValue) {
+        super(context)
+        this.name = name
+        setOrientation(LinearLayout.VERTICAL)
+        createSeekBar(maxValue)
+        createHeader(name)
+
+        addView(header)
+        addView(seekBar)
+    }
+
+    public createHeader(String name){
+        header = new TextView(context)
+        header.setText("$name: $value")
+    }
+
+    public createSeekBar(int maxValue){
+        seekBar = new SeekBar(context)
+        seekBar.setMax(maxValue)
+        seekBar.setMinimumWidth(barSize)
+
+        SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener(){
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                value = seekBar.getProgress()
+                header.setText("$name: $value")
+            }
+        };
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener)
+    }
+
+}
--- app/src/main/java/com/ordonteam/custom/CustomLayout.groovy
+++ app/src/main/java/com/ordonteam/custom/CustomLayout.groovy
@@ -2,22 +2,38 @@ package com.ordonteam.custom
 
 import android.content.Intent
 import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.SeekBar
+import android.widget.TextView
+import com.ordonteam.commons.CenteredLayout
+import com.ordonteam.commons.NamedProgressBar
 import com.ordonteam.maze.MazeActivity
 import groovy.transform.CompileStatic
 
 @CompileStatic
-class CustomLayout extends LinearLayout {
-    CustomLayout(CustomActivity customActivity) {
+class CustomLayout extends CenteredLayout {
+    private int MAX_WIDTH = 100
+    private int MAX_HEIGTH = 2
+    private int MAX_BOT_LEVEL = 10
+    private NamedProgressBar widthProgressBar
+    private NamedProgressBar heightProgressBar
+    private NamedProgressBar botLevelProgressBar
+
+    public CustomLayout(CustomActivity customActivity) {
         super(customActivity)
+        widthProgressBar = new NamedProgressBar(customActivity, "Width", MAX_WIDTH)
+        heightProgressBar = new NamedProgressBar(customActivity, "Height", MAX_HEIGTH)
+        botLevelProgressBar = new NamedProgressBar(customActivity, "Bot Level", MAX_BOT_LEVEL)
+        addView(widthProgressBar)
+        addView(heightProgressBar)
+        addView(botLevelProgressBar)
+
         Button button = new Button(customActivity)
-        button.setText('CustomLayout')
+        button.setText('Create Level')
         button.setOnClickListener({
             Intent intent = new Intent(context, MazeActivity.class)
             intent.putExtra('MazeGenerator', new CustomMazeGenerator())
             customActivity.startActivity(intent)
         })
         addView(button)
-
     }
 }
